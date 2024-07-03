6ae91a1486dd9939827646ce8264efc7398e4cb1
PopyMistry <popymistry7288@gmail.com>
2024-07-03 22:28:43 +0600

    first commit

---
+++ php/1st largest number.php
@@ -0,0 +1,13 @@
+<?php
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+if ($num1 >= $num2 && $num1 >= $num3) {
+    echo "the largest number: " . $num1;
+} elseif ($num2 >= $num1 && $num2 >= $num3) {
+    echo "the largest number: " . $num2;
+} else {
+    echo "the largest number: " . $num3;
+}
+?>
---
+++ php/2nd temperature.php
@@ -0,0 +1,10 @@
+<?php
+$celsius = 32;
+
+// Formula to convert from Celsius to Fahrenheit
+
+$fahrenheit = ($celsius * 9/5) + 32;
+
+echo "Celsius temperature: " . $celsius . "°C\n";
+echo "Fahrenheit temperature: " . $fahrenheit . "°F";
+?>
